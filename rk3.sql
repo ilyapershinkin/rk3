dbc4b5250075f20cda3adeb0ba89eeab57f1f9a0
ilyapershinkin <56293618+ilyapershinkin@users.noreply.github.com>
2019-11-26 11:02:11 +0300

    Update rk3.sql

--- rk3.sql
+++ rk3.sql
@@ -10,18 +10,18 @@ year VARCHAR(20) NOT NULL,
 
 INSERT INTO countrys (`id`,`country`,`city`,`population`,`year`)
 VALUES
-(1,'Russia','Moscow',600000,'123',),
-(2,'Ukraine','Kiev',600000,'234',),
-(3,'Canada','Torronto',100000000,'345',),
-(4,'USA','Washington',200000,'456',),
-(5,'France','Paris',1600000,'567',),
-(6,'Italy','Rome',200000000,'789',),
-(7,'Britain','London',50000,'890',),
-(8,'Brasil','Brasil',2000000,'876',),
-(9,'Germany','Berlin',220000,'321',),
-(10,'Finland','Helsinki',100000,'879',);
+(1,'Russia','Moscow','600000','123',),
+(2,'Ukraine','Kiev','600000','234',),
+(3,'Canada','Torronto','100000000','345',),
+(4,'USA','Washington','200000','456',),
+(5,'France','Paris','1600000','567',),
+(6,'Italy','Rome','200000000','789',),
+(7,'Britain','London','50000','890',),
+(8,'Brasil','Brasil','2000000','876',),
+(9,'Germany','Berlin','220000','321',),
+(10,'Finland','Helsinki','100000','879',);
 
 // 2 задание
-SELECT city, country FROM countrys WHERE population > 1000000;
+SELECT city, country FROM countrys WHERE population > '1000000';
 // 3 задание
-SELECT city FROM countrys WHERE population > 500000 AND population <1000000 AND city LIKE '%M';
+SELECT city FROM countrys WHERE population > '500000' AND population < '1000000' AND city LIKE '%M';
